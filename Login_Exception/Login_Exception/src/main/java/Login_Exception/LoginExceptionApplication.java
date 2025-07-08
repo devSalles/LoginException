46e289d3addab78fa8d68c03df35d589a127cfc6
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-07-08 15:07:32 -0300

    Add files via upload

---
+++ Login_Exception/Login_Exception/src/main/java/Login_Exception/LoginExceptionApplication.java
@@ -0,0 +1,13 @@
+package Login_Exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginExceptionApplication.class, args);
+	}
+
+}
